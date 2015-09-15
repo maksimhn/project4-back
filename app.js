b381b52c02a9e5420f2082a1ecb2ec52db30c629
Maksim <dollydasheep@gmail.com>
2015-09-14 21:48:51 -0400

    CORS fix

--- app.js
+++ app.js
@@ -21,6 +21,24 @@ var cors = require('cors');
 
 var app = express();
 
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+};
+
+app.configure(function () {
+  app.use(allowCrossDomain);
+});
+
 // view engine setup
 // app.set('views', path.join(__dirname, 'views'));
 // app.set('view engine', 'hbs');
