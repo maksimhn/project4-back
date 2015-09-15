6ee5946025fbc787f7bcb693b7f8c899a606d82e
Maksim <dollydasheep@gmail.com>
2015-09-15 14:41:06 -0400

    CORS fix test

--- app.js
+++ app.js
@@ -84,6 +84,7 @@ app.use(passport.session());
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "http://maksimhn.github.io");
+  res.header("Access-Control-Allow-Credentials", "true");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
