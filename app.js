09bd180e3b77cc0621045c3295d93ed00380a924
Maksim <dollydasheep@gmail.com>
2015-09-14 21:39:34 -0400

    CORS fix

--- app.js
+++ app.js
@@ -25,11 +25,11 @@ var app = express();
 // app.set('views', path.join(__dirname, 'views'));
 // app.set('view engine', 'hbs');
 
-app.use(function(req, res, next) {
+app.all('/', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
   next();
-});
+ });
 
 // app.use(cors({
 //  credentials: true,
