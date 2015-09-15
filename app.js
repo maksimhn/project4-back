5223bba6b9b38ba38363fd3c7706338851c19bc6
Maksim <dollydasheep@gmail.com>
2015-09-14 21:32:59 -0400

    CORS fix

--- app.js
+++ app.js
@@ -25,12 +25,18 @@ var app = express();
 // app.set('views', path.join(__dirname, 'views'));
 // app.set('view engine', 'hbs');
 
-app.use(cors({
- credentials: true,
- origin: true
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+// app.use(cors({
+//  credentials: true,
+//  origin: true
  // allowedMethods: 'POST, GET, PUT, DELETE, OPTIONS'
  // allowedHeaders: ['Cookie', 'Content-Type']
-}));
+// }));
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
