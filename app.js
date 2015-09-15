f411911a271f8fbe58ab71dea422923bf81826c9
Maksim <dollydasheep@gmail.com>
2015-09-14 21:50:40 -0400

    CORS fix test

--- app.js
+++ app.js
@@ -82,11 +82,11 @@ app.use(passport.initialize());
 // mount return value of `passport.session` invocation on `app`
 app.use(passport.session());
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
 
 
 app.use('/', routes);
