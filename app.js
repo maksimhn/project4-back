69a232571512c8f35ff0e78d49651ecb35c30943
Maksim <dollydasheep@gmail.com>
2015-09-16 13:57:37 -0400

    Attempt to allow DELETE action

--- app.js
+++ app.js
@@ -21,34 +21,6 @@ var cors = require('cors');
 
 var app = express();
 
-// var allowCrossDomain = function(req, res, next) {
-//     res.header('Access-Control-Allow-Origin', '*');
-//     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-//     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-
-//     // intercept OPTIONS method
-//     if ('OPTIONS' == req.method) {
-//       res.send(200);
-//     }
-//     else {
-//       next();
-//     }
-// };
-
-// app.configure(function () {
-//   app.use(allowCrossDomain);
-// });
-
-// view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'hbs');
-
-// app.all('/', function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Headers", "X-Requested-With");
-//   next();
-//  });
-
 // app.use(cors({
 //  credentials: true,
 //  origin: true
@@ -85,6 +57,7 @@ app.use(passport.session());
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "http://maksimhn.github.io");
   res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS'")
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
